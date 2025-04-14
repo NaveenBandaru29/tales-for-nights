a34d9aff6f4ced0e08d44e26eebdb21ce0ebf108
NaveenBandaru29 <bandarun784@gmail.com>
2025-04-14 19:39:27 +0530

    fix: fixed dark background bug in tale detail

--- app/components/common/CustomEditor/EditorTextReadOnly.tsx
+++ app/components/common/CustomEditor/EditorTextReadOnly.tsx
@@ -5,9 +5,9 @@ import useExtensions from './useExtensions'
 const EditorTextReadOnly = ({ content }: { content: string }) => {
     const extensions = useExtensions()
     return (
-        <div className="prose max-w-none prose-blockquote:border-none">
+        <>
             <RichTextReadOnly immediatelyRender={false} extensions={extensions} content={content} />
-        </div>
+        </>
     )
 }
 
--- app/components/raw/RawForm.tsx
+++ app/components/raw/RawForm.tsx
@@ -6,7 +6,7 @@ import { Suspense, useState } from 'react';
 import {MultiValue} from "react-select"
 import SelectField from '../ui/SelectField';
 import { Loader } from '../ui/Loader';
-const CustomEditor = dynamic(()=>import('../common/CustomEditor'),{ssr:false})
+const CustomEditor = dynamic(()=>import('../common/CustomEditor'),{ssr:false,loading:()=><Loader loadingText='Loading Editor...'/>})
 
 export interface Tag {
   value: string;
@@ -67,7 +67,6 @@ export default function RawForm({handleFormClose}:{handleFormClose:()=>void}) {
           rows={3}
           required
           /> */}
-        <Suspense fallback={<Loader />}>
           <CustomEditor
             placeholder="Enter RAW content..."
             showPreview
@@ -75,7 +74,6 @@ export default function RawForm({handleFormClose}:{handleFormClose:()=>void}) {
             setContent={setContent}
             
           />
-        </Suspense>
         <SelectField
           isMulti
           isClearable
